daaaa5ca33f1b4855f7b44cb878307eccbe6ec62
jslirola <jslirola@andalucia-devs.com>
2017-03-19 01:05:55 +0100

    Salt in SHA1

--- js/common.js
+++ js/common.js
@@ -1,7 +1,12 @@
 function hashPassword()
 {
+    var p = $("#inputPassword").val();
+    var s = $("#inputSalt").val()
 
-    var sha1 = CryptoJS.SHA1($("#inputPassword").val());
+    if(!s)
+        var sha1 = CryptoJS.SHA1(p);
+    else
+        var sha1 = CryptoJS.SHA1(p + s);
+
     $("textarea#result").val("SHA1: " + sha1.toString());
-
 }
