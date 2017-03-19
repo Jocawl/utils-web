f446d7d2aed2c014e7888c6eef77d2ee3039d96a
jesusce21 <jesusctic@gmail.com>
2017-03-19 18:44:57 +0100

    DEV: Added new file

--- js/common.js
+++ js/common.js
@@ -42,3 +42,49 @@ var binto = function(number, type) {
         return (dec).toString(8)
     }
 }
+
+var setValues = function(identificator, value){
+    switch(identificator) {
+        case "decto":
+            $("#hexto").val(decto(value, 'hex'))
+            $("#octto").val(decto(value, 'oct'))
+            $("#binto").val(decto(value, 'bin'))
+            break;
+        case "hexto":
+            $("#decto").val(hexto(value, 'dec'))
+            $("#octto").val(hexto(value, 'oct'))
+            $("#binto").val(hexto(value, 'bin'))
+            break;
+        case "octto":
+            $("#decto").val(octto(value, 'dec'))
+            $("#hexto").val(octto(value, 'hex'))
+            $("#binto").val(octto(value, 'bin'))
+            break;
+        case "binto":
+            $("#decto").val(binto(value, 'dec'))
+            $("#hexto").val(binto(value, 'hex'))
+            $("#octto").val(binto(value, 'oct'))
+            break;    
+    }
+}
+
+var createForm = function(structure, id="form"){
+    ident = "#"+id;
+    $.each(structure, function( k, v ) {
+        obj = $('<div>', {class: 'form-group'})
+        obj.append($('<label>', {class: 'control-label', for: k, text: v["label"]}))
+        obj.append($('<input>', {class: 'form-control number', type: 'text', pattern: v["regexForm"], id: k, placeholder: v["label"]}))
+
+        $(ident).append(obj);
+        
+    });
+
+    $(ident).append($('<input>', {class: 'form-control number', type: 'hidden', id: 'lastFocus'}))
+
+    obj = $('<div>', {class: 'form-group col-sm-offset-2 col-sm-10'})
+    obj.append($('<button>', {class: 'btn btn-default', type: 'submit', text: 'Submit'}))
+
+    $(ident).append(obj);
+
+    
+}
